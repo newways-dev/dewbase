8a1ff3ebf92c0ead28ba5ecef0586c7d8cb03bee
Kate <kate.dew.next@gmail.com>
2026-04-30 20:24:05 +0200

    background jobs with inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
-import { requireAuth } from '@/lib/auth-utils'
-import { caller } from '@/trpc/server'
+'use client'
 
-const Page = async () => {
-  await requireAuth()
+import { Button } from '@/components/ui/button'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useTRPC } from '@/trpc/client'
+import { toast } from 'sonner'
 
-  await caller.getUsers()
+const Page = () => {
+  const trpc = useTRPC()
+  const queryClient = useQueryClient()
+  const { data } = useQuery(trpc.getWorkflows.queryOptions())
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success('Job queued')
+      },
+    }),
+  )
 
   return (
-    <div className="min-h-screen min-w-screen flex justify-center items-center">
-      Protected
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+      protected server component
+      <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create workflow
+      </Button>
     </div>
   )
 }
